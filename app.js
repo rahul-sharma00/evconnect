3612671b2af8f9b9c721f9efde1e3cb85384fa37
Rahul Sharma <131450041+rahul-sharma00@users.noreply.github.com>
2024-03-29 04:17:01 -0700

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let menu=document.getElementById("menu");
-let nav=document.querySelector(".navigations")
-menu.addEventListener("click",()=>{
-    if(nav.style.visibility=="hidden"){
-        nav.style.visibility="visible";
-    }else{
-        nav.style.visibility="hidden";
-    }
-})
